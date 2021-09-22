0a6ea0b4ad7693ccca79840a38a5a229f73bbc80
Yiğit Sadıç <yigitsadic@gmail.com>
2021-09-22 19:47:41 +0300

    Removed scaffold controllers.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CitiesModule } from './cities/cities.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CitiesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
