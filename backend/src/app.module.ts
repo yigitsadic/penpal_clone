98160204c363aa92af0494a1e5e89bdc03bf5bbb
Yiğit Sadıç <yigitsadic@gmail.com>
2021-09-25 22:55:36 +0300

    removed mess

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CitiesModule } from './cities/cities.module';
 import { LanguagesModule } from './languages/languages.module';
-import { NotificationsService } from './notifications/notifications.service';
-import { NotificationsController } from './notifications/notifications.controller';
 import { NotificationsModule } from './notifications/notifications.module';
-import { TemplatesController } from './templates/templates.controller';
-import { TemplatesService } from './templates/templates.service';
 import { TemplatesModule } from './templates/templates.module';
 import { ConversationsModule } from './conversations/conversations.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RegistrationsModule } from './registrations/registrations.module';
 
 @Module({
   imports: [
@@ -29,8 +26,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TemplatesModule,
     ConversationsModule,
     UsersModule,
+    RegistrationsModule,
   ],
-  controllers: [NotificationsController, TemplatesController],
-  providers: [NotificationsService, TemplatesService],
 })
 export class AppModule {}
