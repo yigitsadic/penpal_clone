f4c61969fe27c76d24de9e64cdcaf5d79b06dbe7
Yiğit Sadıç <yigitsadic@gmail.com>
2021-09-25 15:12:07 +0300

    Created languages table.

---
+++ backend/migrations/1632571287321-createLanguagesTable.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, Table, TableIndex } from 'typeorm';
+
+export class createLanguagesTable1632571287321 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'languages',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'localName',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'shortCode',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createIndex(
+      'languages',
+      new TableIndex({
+        name: 'IDX_LANGUAGE_NAME',
+        columnNames: ['name'],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('languages', true);
+  }
+}
