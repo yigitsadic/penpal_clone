05204c230303b0adc7931a65667aef952e19f821
Yiğit Sadıç <yigitsadic@gmail.com>
2021-09-26 22:05:26 +0300

    Added endpoints for people that logged in user messaged and messages sent and received for that user.

--- backend/src/messages/messages.controller.ts
+++ backend/src/messages/messages.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './create-message.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
@@ -7,6 +16,21 @@ import { Request } from 'express';
 @Controller('messages')
 export class MessagesController {
   constructor(private service: MessagesService) {}
+
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  conversations(@Req() req: Request) {
+    return this.service.relatedFor(req.user.id);
+  }
+
+  @Get('/:id')
+  @UseGuards(JwtAuthGuard)
+  conversationDetail(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Req() req: Request,
+  ) {
+    return this.service.conversationDetail(id, req.user.id);
+  }
 
   @Post()
   @UseGuards(JwtAuthGuard)
--- backend/src/messages/messages.service.ts
+++ backend/src/messages/messages.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Message } from './message.entity';
-import { Repository } from 'typeorm';
+import { getManager, Repository } from 'typeorm';
 import { CreateMessageDto } from './create-message.dto';
 
 @Injectable()
@@ -12,9 +12,36 @@ export class MessagesService {
   ) {}
 
   async relatedFor(userId: string) {
+    const entityManager = getManager();
+    return await entityManager.query(
+      `
+    select
+users.id,
+users."firstName",
+users."lastName"
+from users
+where users.id in (
+select distinct messages."senderId" from messages
+where messages."receiverId" = $1
+union all
+select distinct messages."receiverId" from messages
+where messages."senderId" = $1
+)`,
+      [userId],
+    );
+  }
+
+  async conversationDetail(participant1: string, participant2: string) {
     return await this.repository
       .createQueryBuilder()
-      .where('senderId=:userId or receiverId=:userId', { userId })
+      .where(
+        '("senderId" = :participant1 and "receiverId" = :participant2) or ("senderId" = :participant2 and "receiverId" = :participant1)',
+        {
+          participant1,
+          participant2,
+        },
+      )
+      .orderBy('"createdAt"', 'DESC')
       .getMany();
   }
 
