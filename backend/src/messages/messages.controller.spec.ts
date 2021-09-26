0f13536de13df12f75403154506e4065aa45c957
Yiğit Sadıç <yigitsadic@gmail.com>
2021-09-26 20:12:18 +0300

    Added tests for messages controller.

--- backend/src/messages/messages.controller.spec.ts
+++ backend/src/messages/messages.controller.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MessagesController } from './messages.controller';
+import { MessagesService } from './messages.service';
+import { CreateMessageDto } from './create-message.dto';
+import * as mocks from 'node-mocks-http';
+
+const mockMessagesService = {
+  create: jest.fn().mockImplementation(),
+};
 
 describe('MessagesController', () => {
+  const req = mocks.createRequest();
+  req.user = { id: '12312312' };
+
   let controller: MessagesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MessagesController],
+      providers: [
+        {
+          provide: MessagesService,
+          useValue: mockMessagesService,
+        },
+      ],
     }).compile();
 
     controller = module.get<MessagesController>(MessagesController);
@@ -14,5 +30,15 @@ describe('MessagesController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should insert given message', async () => {
+    const dto = new CreateMessageDto();
+    dto.content = 'Lorem';
+    dto.receiverId = '12312321';
+
+    await controller.create(dto, req);
+
+    expect(mockMessagesService.create).toHaveBeenCalledWith('12312312', dto);
   });
 });
--- backend/src/messages/messages.service.spec.ts
+++ backend/src/messages/messages.service.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MessagesService } from './messages.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Message } from './message.entity';
+import { CreateMessageDto } from './create-message.dto';
+
+const mockMessageRepo = {
+  create: jest.fn().mockImplementation(),
+  save: jest.fn().mockImplementation(),
+};
 
 describe('MessagesService', () => {
   let service: MessagesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MessagesService],
+      providers: [
+        MessagesService,
+        {
+          provide: getRepositoryToken(Message),
+          useValue: mockMessageRepo,
+        },
+      ],
     }).compile();
 
     service = module.get<MessagesService>(MessagesService);
@@ -14,5 +28,12 @@ describe('MessagesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should save message to repository', async () => {
+    await service.create('12312', new CreateMessageDto());
+
+    expect(mockMessageRepo.create).toHaveBeenCalled();
+    expect(mockMessageRepo.save).toHaveBeenCalled();
   });
 });
